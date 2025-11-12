664c974dcf1185b8269f18f3e16b429934baafec
MBANS8A1 <seanmbng@aol.com>
2025-11-12 08:13:30 +0000

    I created three pieces of state for the currency calculator; the amount(quantity of money being converted), the starting "from" currency and the final end currency.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, toFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
   return (
     <div className="App">
       <input type="text" />
+
       <select>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
